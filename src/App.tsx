745cf88b29d329c2d559f950be5f16039c26767d
marcostenoriodev <marcosvtenoriodev@gmail.com>
2024-04-22 09:45:45 -0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import Router from "./routes/Routes";
+import { Home } from "lucide-react";
+import { Outlet, BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/shared/Navbar";
+import Publication from "./pages/publication/publication";
+import Test from "./pages/testPage/test";
+
+function Layout() {
+  return (
+    <>
+      <NavBar/>
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
     <>
-      <Router/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/t" element={<Test />} />
+          <Route path="/publication" element={<Publication />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
