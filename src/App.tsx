98c07cedc602c0624e075db1877b1e5c220d8c7b
marcostenoriodev <marcosvtenoriodev@gmail.com>
2024-05-01 17:34:39 -0300

    created login page and guard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import  Home  from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/shared/Navbar";
 import Publication from "./pages/publication/publication";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import { Auth } from "./pages/auth/Auth";
+
+function Layout({ children }: any) {
+  return (
+    <>
+      <SignedOut>
+        <Auth />
+      </SignedOut>
+      <SignedIn>
+        <NavBar />
+        {children}
+      </SignedIn>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<><NavBar /><Home /></>} />
-          <Route path="/publication" element={<><NavBar /><Publication /></>} />
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="/publication"
+            element={
+              <Layout>
+                <Publication />
+              </Layout>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/auth/Auth.tsx
@@ -0,0 +1,18 @@
+import { SignIn } from "@clerk/clerk-react"
+import Logo from '../../assets/images/logo.png'
+
+
+export function Auth() {
+  return (
+    <div className="flex justify-between w-screen h-screen">
+        <div className="hidden w-5/12 bg-muted md:flex md:flex-col md:justify-center md:items-center">
+        <img src={Logo} alt="logo" className="h-11/12 w-11/12 xl:h-8/12 xl:w-8/12 max-w-[500px] object-cover dark:brightness-[0.2] dark:grayscale" />
+        <h1 className="text-6xl text-center">Mestres Online</h1>
+      </div>
+      <div className="flex md:w-7/12 items-center justify-center py-12 bg-primary w-screen md:rounded-l-3xl">
+        <SignIn/>
+      </div>
+      
+    </div>
+  )
+}
