73ad30c93ba90715a849920bd41a408620e1e0dc
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-07-29 19:20:06 -0300

    Style: Fix style in PostPreview component

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -4,17 +4,22 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Card, CardHeader } from "./ui/card";
 
 const PostPreview = ({ post }: { post: IPost }) => {
-	console.log(post);
 	return (
-		<Card className="lg:max-w-[1000px] px-10 py-6 lg:flex-row items-center justify-between">
+		<Card className="lg:max-w-[1000px] px-4 sm:px-10 py-6 lg:flex-row items-center justify-between">
 			<div className="w-full flex justify-end">
-				<div className={`${post.isActive ? 'bg-green-500/30 text-green-600':'bg-red-500/30 text-red-600'} py-1 px-6 rounded-full font-semibold`}>
-				{post.isActive ? "Ativo" : "inativo"}
+				<div
+					className={`${
+						post.isActive
+							? "bg-green-500/30 text-green-600"
+							: "bg-red-500/30 text-red-600"
+					} py-1 px-6 rounded-full font-semibold mb-4`}
+				>
+					{post.isActive ? "Ativo" : "inativo"}
 				</div>
 			</div>
-			<div className="flex flex-col lg:flex-row items-center justify-between w-full">
-				<div className="max-w-[500px] mr-6">
-					<CardHeader className="flex px-0">
+			<div className="flex flex-col xl:flex-row items-center xl:items-start justify-between w-full">
+				<div className="mr-6 w-full sm:w-auto sm:mx-0 mx-auto">
+					<CardHeader className="flex sm:flex-row sm:gap-3 px-0 xl:p-0">
 						<Avatar className="w-14 h-14 mx-auto sm:mx-0">
 							<AvatarImage
 								src={post.author.imageProfile}
@@ -24,10 +29,12 @@ const PostPreview = ({ post }: { post: IPost }) => {
 						</Avatar>
 						<div className="ml-1">
 							<div className="flex flex-col sm:flex-row items-center gap-1">
-								<h4 className="">{post.author.name}</h4>
-								<p className="text-xs text-muted-foreground">
-									- {post.author.office && post.author.office}
-								</p>
+								<div className="flex flex-nowrap justify-center items-center">
+									<h4 className="mr-1">{post.author.name}</h4>
+									<p className="text-xs text-muted-foreground">
+										- {post.author.office && post.author.office}
+									</p>
+								</div>
 								<i className="flex items-center justify-center">
 									<DotFilledIcon className="w-[10px] text-muted-foreground" />
 								</i>
@@ -61,10 +68,10 @@ const PostPreview = ({ post }: { post: IPost }) => {
 						</div>
 					</CardHeader>
 					<div>
-						<h2 className="text-md sm:text-lg md:text-2xl font-bold mt-4">
+						<h2 className="text-md text-center md:text-start sm:text-lg md:text-2xl font-bold mt-4">
 							{post.title}
 						</h2>
-						<h3 className="text-xs sm:text-sm font-normal mt-4">
+						<h3 className="text-xs text-center md:text-start  sm:text-sm font-normal mt-4">
 							{post.contentPreview}
 						</h3>
 					</div>
@@ -73,7 +80,7 @@ const PostPreview = ({ post }: { post: IPost }) => {
 					<img
 						src={post.imagePreview}
 						alt="pré visualização da imagem"
-						className="w-[350px] aspect-video md:min-w-[350px] mt-5 lg:mt-0"
+						className="w-[350px] aspect-video md:min-w-[350px] mt-5 xl:mt-0"
 					/>
 				</div>
 			</div>
