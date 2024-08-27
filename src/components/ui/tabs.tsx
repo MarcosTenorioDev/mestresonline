58dd9bc27d665966be35aa8e8fe0a017270f2cf1
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-08-27 17:36:54 -0300

    Feat: update tabs component

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,39 +1,121 @@
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
+import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
 const Tabs = TabsPrimitive.Root
 
+
+const TabsListVariants = cva(
+  " inline-flex items-center justify-start h-9",
+  {
+    variants: {
+      variant: {
+        default:
+          "rounded-lg bg-muted/30 p-1",
+        underline:
+          "border-b rounded-none bg-background p-0",
+      },
+      size: {
+        default: "h-9",
+        sm: "h-8  text-xs",
+        lg: "h-10 ",
+        icon: "h-9 w-9",
+      },
+      width:{
+        default:"w-full",
+        fit:"w-fit"
+      }
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+      width:"default"
+    },
+  }
+)
+
+
+const TabsTriggerVariants = cva(
+    "inline-flex items-center justify-center whitespace-nowrap font-normal  transition-all disabled:pointer-events-none data-[state=active]:text-foreground px-3",
+    {
+      variants: {
+        variant: {
+          default:
+            "data-[state=active]:bg-background data-[state=active]:text-primary data-[state=active]:font-bold text-current ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[state=active]:shadow disabled:opacity-50 rounded-md py-1",
+          underline:
+            "bg-background border-b-2 text-muted-foreground border-background focus:border-primary data-[state=active]:font-bold ring-0 outline-none shadow-none data-[state=active]:shadow-none data-[state=active]:border-b-2 data-[state=active]:border-primary disabled:opacity-100 data-[state=active]:shadow-none rounded-none m-0 pt-1.5 pb-2 hover:bg-background-muted",
+        },
+        size: {
+          default: "text-sm",
+          sm: " text-xs",
+          lg: "text-md",
+          icon: "h-9 w-9",
+        },
+        width:{
+          default:"w-full",
+          fit:"w-fit"
+        }
+      },
+      defaultVariants: {
+        variant: "default",
+        size: "default",
+        width:"default"
+      },
+    }
+  )
+
+
+ 
+
+  export interface TabsListProps extends React.ComponentPropsWithoutRef<typeof TabsPrimitive.List> {
+    variant?: "default"|"underline" | null | undefined;
+    size?: "default" | "sm" | "lg" | "icon" | null | undefined;
+    width?: "default" | "fit" | null | undefined;
+  }
+  
+  
+
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
+  TabsListProps
+>(({ className,variant,size,width, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
-      className
+      TabsListVariants({ variant,size,width, className })
     )}
     {...props}
   />
 ))
 TabsList.displayName = TabsPrimitive.List.displayName
 
+export interface TabsTriggerProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof TabsTriggerVariants> {
+  asChild?: boolean
+}
+
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
-      className
-    )}
-    {...props}
-  />
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+  TabsTriggerProps
+>(({ className, variant, size, width, ...props }, ref) => {
+  const { value, ...restProps } = props as { value: string } & React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>;
+
+  return (
+    <TabsPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        TabsTriggerVariants({ variant, size, width, className })
+      )}
+      value={value}
+      {...restProps}
+    />
+  );
+});
+
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
